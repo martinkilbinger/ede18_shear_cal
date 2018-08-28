f33a3860020157e63bfe1e23b2bf00c3d8ddd1d1
Martin Kilbinger <martin.kilbinger@cea.fr>
2018-08-28 17:51:01 +0200

    modified run multi shear script

--- scripts_for_shear_bias/galsim/run_multishear.sh
+++ scripts_for_shear_bias/galsim/run_multishear.sh
@@ -2,7 +2,8 @@ g1=-0.02
 g2=0.0
 
 #dirname='/home/apujol/galsim/multishear/g1_'$g1'_g2_'$g2
-dirname='/dsm/cosmo02/sparseastro/apujol/galsim/multishear/g1_'$g1'_g2_'$g2
+#dirname='/dsm/cosmo02/sparseastro/apujol/galsim/multishear/g1_'$g1'_g2_'$g2
+dirname='output/g1_'$g1'_g2_'$g2
 galsim csc_multishear.yaml gal.shear.g1=$g1 gal.shear.g2=$g2 output.dir=$dirname
 
 
